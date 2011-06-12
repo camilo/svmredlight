2d044b691a640f3b1ce4984f7040061a1259be19
Camilo Lopez <clopez@websense.com>
2011-06-12 19:19:30 -0400

    splitting tests logically

---
+++ test/test_document.rb
@@ -0,0 +1,19 @@
+require './test/helper'
+require './lib/svmredlight'
+
+include SVMLight
+
+class TestDocument < Test::Unit::TestCase
+  def test_create
+    assert d = Document.create(0, 0.5, [1.0, 0, 0, 0, 0.5])
+  end
+
+  def test_create_with_no_array
+    assert_raise(TypeError) { Document.create(-1, 0, {})  }
+  end
+
+  def test_create_with_empty_array
+    assert_raise(ArgumentError) { Document.create(-1, 0, [])}
+  end
+end
+
--- test/test_model.rb
+++ test/test_model.rb
@@ -1,22 +1,6 @@
 require './test/helper'
 require './lib/svmredlight'
 
-include SVMLight
-
-class TestDocument < Test::Unit::TestCase
-  def test_create
-    assert d = Document.create(0, 0.5, [1.0, 0, 0, 0, 0.5])
-  end
-
-  def test_create_with_no_array
-    assert_raise(TypeError) { Document.create(-1, 0, {})  }
-  end
-
-  def test_create_with_empty_array
-    assert_raise(ArgumentError) { Document.create(-1, 0, [])}
-  end
-end
-
 class TestModel < Test::Unit::TestCase
   def test_read
     assert m = Model.read_from_file('test/model')
